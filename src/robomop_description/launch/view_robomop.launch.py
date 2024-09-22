a16143cda4396535f32cdb6cc122d81a0d3f7188
Jack Rivera <jackarivera@gmail.com>
2024-09-22 15:29:48 -0400

    Update URDF

--- src/robomop_description/launch/view_robomop.launch.py
+++ src/robomop_description/launch/view_robomop.launch.py
@@ -1,73 +1,50 @@
+from ament_index_python.packages import get_package_share_path
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import Command, LaunchConfiguration
 
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    # Declare arguments
-    declared_arguments = []
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="robomop_description",
-            description="Description package of the robomop. Usually the argument is not set, \
-        it enables use of a custom description.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "prefix",
-            default_value='""',
-            description="Prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
-        have to be updated.",
-        )
-    )
+    rover_path = get_package_share_path('robomop_description')
+    default_model_path = rover_path / 'urdf/robomop.urdf.xacro'
+    default_rviz_config_path = rover_path / 'rviz/robomop.rviz'
 
-    # Initialize Arguments
-    description_package = LaunchConfiguration("description_package")
-    prefix = LaunchConfiguration("prefix")
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                                      description='Absolute path to robot urdf file')
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+                                     description='Absolute path to rviz config file')
 
-    # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", "robomop.urdf.xacro"]
-            ),
-            " ",
-            "prefix:=",
-            prefix,
-            " ",
-        ]
-    )
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+                                       value_type=str)
 
-    robot_description = {"robot_description": robot_description_content}
-
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "rviz", "robomop.rviz"]
-    )
     robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
     )
 
-    return LaunchDescription(
-        declared_arguments
-        + [
-            robot_state_publisher_node,
-            rviz_node,
-        ]
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher'
     )
+
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        model_arg,
+        rviz_arg,
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node
+    ])
